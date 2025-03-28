31c3a3650e1eca358ead8797e4ad9f503944cb85
Sandeep <sandeep836815@gmail.com>
2025-03-28 04:26:03 -0700

    Added .gitignore and initial project files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-red-500'>
+      This is a React Page and Tailwindcss is working fine
+    </div>
+  )
+}
+
+export default App
